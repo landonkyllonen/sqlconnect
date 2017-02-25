efe0d45f0e776c0a0c971d1548d5df7201491d06
landon <landonkyllonen@hotmail.com>
2017-02-25 15:15:17 -0800

    LogsTab Layout complete but may need editing. Log creation not yet implemented.

--- SQLConnect/SQLConnect/LogsTab.cs
+++ SQLConnect/SQLConnect/LogsTab.cs
@@ -4,7 +4,7 @@ using Xamarin.Forms;
 
 namespace SQLConnect
 {
-	public class LogsTab
+	public class LogsTab:ContentPage
 	{
 		public LogsTab()
 		{
@@ -13,25 +13,36 @@ namespace SQLConnect
 			//Load logs and initialize list.
 			ObservableCollection<LogListItem> logs = Statics.Default.getLogs();
 
-			ListView logList = new ListView { 
-				RowHeight= 80
+			logs.Add(new LogListItem { logTitle = "TestLog", logDate = "2017-01-01", logMeds = new string[] { "med1", "med2", "med3" }, logText = "sample text", logPublic = false, logImportant = true });
+			logs.Add(new LogListItem { logTitle = "TestLog", logDate = "2017-01-01", logMeds = new string[] { "med1", "med2", "med3" }, logText = "sample text", logPublic = true, logImportant = false });
+			logs.Add(new LogListItem { logTitle = "TestLog", logDate = "2017-01-01", logMeds = new string[] { "med1", "med2", "med3" }, logText = "sample text", logPublic = true, logImportant = true });
+
+
+			ListView logList = new ListView
+			{
+				RowHeight = 80
 			};
+			logList.ItemsSource = logs;
 
 			//Create row layouts
 			var logDataTemplate = new DataTemplate(() =>
 			{
 				RelativeLayout templateHolder = new RelativeLayout { HorizontalOptions = LayoutOptions.Fill, VerticalOptions = LayoutOptions.Fill };
 
-				var title = new Label { FontSize = 15, TextColor = Color.Teal, VerticalTextAlignment = TextAlignment.Center, HorizontalTextAlignment = TextAlignment.Center };
-				var date = new Label { FontSize = 15, TextColor = Color.Teal, VerticalTextAlignment = TextAlignment.Center, HorizontalTextAlignment = TextAlignment.Center };
+				var title = new Label { FontSize = 15, TextColor = Color.Teal, VerticalTextAlignment = TextAlignment.Center, HorizontalTextAlignment = TextAlignment.Start };
+				var date = new Label { FontSize = 15, TextColor = Color.Teal, VerticalTextAlignment = TextAlignment.Center, HorizontalTextAlignment = TextAlignment.Start };
+				var visibility = new Image { Aspect = Aspect.Fill, Source="group.png" };
+				var important = new Image { Aspect = Aspect.Fill, Source="important.png" };
 
 				title.SetBinding(Label.TextProperty, "logTitle");
 				date.SetBinding(Label.TextProperty, "logDate");
+				visibility.SetBinding(IsVisibleProperty, "logPublic");
+				important.SetBinding(IsVisibleProperty, "logImportant");
 
 				templateHolder.Children.Add(title, Constraint.Constant(15), Constraint.Constant(10),
 									Constraint.RelativeToParent((parent) =>
 									{
-										return parent.Width - 15;
+										return parent.Width /2 - 15;
 									}),
 									Constraint.RelativeToParent((parent) =>
 									{
@@ -40,7 +51,7 @@ namespace SQLConnect
 				templateHolder.Children.Add(date, Constraint.Constant(15),
 									Constraint.RelativeToParent((parent) =>
 									{
-										return parent.Height/2;
+										return parent.Height / 2;
 									}),
 									Constraint.RelativeToParent((parent) =>
 									{
@@ -51,10 +62,94 @@ namespace SQLConnect
 										return parent.Height / 2 - 10;
 									}));
 
+				templateHolder.Children.Add(visibility, Constraint.RelativeToParent((parent) => { return parent.Width *3/4-15; }),
+											Constraint.RelativeToParent((parent) => { return parent.Height / 2 - 15; }),
+											Constraint.Constant(30), Constraint.Constant(30));
+
+				templateHolder.Children.Add(important, Constraint.RelativeToParent((parent) => { return parent.Width*3/4+30; }),
+											Constraint.RelativeToParent((parent) => { return parent.Height / 2 - 15; }),
+											Constraint.Constant(30), Constraint.Constant(30));
+
 				return new ViewCell { View = templateHolder };
 			});
 
 			logList.ItemTemplate = logDataTemplate;
+
+			holder.Children.Add(logList, Constraint.Constant(0), Constraint.Constant(0),
+								Constraint.RelativeToParent((parent) =>
+								{
+									return parent.Width;
+								}),
+								Constraint.RelativeToParent((parent) =>
+								{
+									return parent.Height * .8;
+								}));
+
+			//Now create bottom bar
+			RelativeLayout bottom = new RelativeLayout { BackgroundColor = Color.FromHex("#009a9a") };
+
+			Button createLog = new Button
+			{
+				Text = "+ Create Log",
+				TextColor = Color.White,
+				BackgroundColor = Color.FromHex("#009a9a")
+			};
+			createLog.Clicked += toCreateLog;
+
+			string[] sortTypes = new string[] { "Date", "Public", "Importance" };
+			Picker sorting = new Picker
+			{
+				TextColor = Color.White,
+				BackgroundColor = Color.FromHex("#009a9a")
+			};
+
+			foreach (string s in sortTypes)
+			{
+				sorting.Items.Add(s);
+			}
+
+			Label sortLbl = new Label { TextColor = Color.White, Text = "Sort By:", HorizontalTextAlignment=TextAlignment.Center, VerticalTextAlignment=TextAlignment.Center };
+
+			bottom.Children.Add(createLog, Constraint.Constant(0), Constraint.Constant(0),
+									Constraint.RelativeToParent((parent) =>
+									{
+										return parent.Width *2/3;
+									}),
+									Constraint.RelativeToParent((parent) =>
+									{
+										return parent.Height;
+									}));
+
+			bottom.Children.Add(sortLbl, Constraint.RelativeToParent((parent) => { return parent.Width * 2 / 3; }), Constraint.Constant(0),
+									Constraint.RelativeToParent((parent) =>
+									{
+										return parent.Width * 1 / 3;
+									}),
+									Constraint.RelativeToParent((parent) =>
+									{
+										return parent.Height * 1 / 3;
+									}));
+
+			bottom.Children.Add(sorting, Constraint.RelativeToParent((parent) => { return parent.Width*2/3+8; }), Constraint.RelativeToParent((parent) => { return parent.Height * 1 / 3; }),
+									Constraint.RelativeToParent((parent) =>
+									{
+										return parent.Width * 1 / 3-16;
+									}),
+									Constraint.RelativeToParent((parent) =>
+									{
+										return parent.Height*2/3-16;
+									}));
+
+			holder.Children.Add(bottom, Constraint.Constant(0), Constraint.RelativeToParent((parent) => { return parent.Height * 0.8; }),
+								Constraint.RelativeToParent((parent) => { return parent.Width; }),
+			                    Constraint.RelativeToParent((parent) => { return parent.Height * 0.2; }));
+
+			Content = holder;
+		}
+
+		public void toCreateLog(object s, EventArgs e)
+		{
+			//Navigation.PushModalAsync(new CreateLogPage());
 		}
 	}
 }
